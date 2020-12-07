5b21f33784af802d76809fc9901e9d9f1b8fcfcd
l38537 <bekus2004@mail.ru>
2020-12-07 18:28:09 +0300

    ...

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,14 @@
 //јлександр Ѕекус
-//создать класс BankAccount (банковский счет) с атрибутами фамили€, им€, баланс, личный номер
-//ћетоды - конструктор по умолчанию, конструктор с параметрами, get, set, print, положить деньги на счЄт, сн€ть деньги со счета.
-//ѕредусмотреть, что со счета невозможно сн€ть денег больше, чем там есть.
+// Alexander Bekus
+// create class BankAccount (bank account) with attributes last name, first name, balance, personal number
+// Methods - default constructor, constructor with parameters, get, set, print, put money into the account, withdraw money from the account.
+// Provide that it is impossible to withdraw more money from the account than there is.
 #include <iostream>
 #include <string>
 #include "BankAccount.h"
 using namespace std;
 int main() {
+	setlocale(LC_ALL, "Rus");
 	BankAccount a("Gleb", "Tereshkin" ,100 ,"7637905A001PB6");
 	a.print();
 	a.TopUpBalance(100);
